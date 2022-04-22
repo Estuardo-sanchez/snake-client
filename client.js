5adcdc0f7d3955751320bbfd6796480057edbc2f
Estuardo-sanchez <93778202+Estuardo-sanchez@users.noreply.github.com>
2022-04-22 15:02:04 -0400

    refactored and used constants

--- client.js
+++ client.js
@@ -1,29 +1,20 @@
 const net = require("net");
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.3',
-    port: 50541
+const { IP, PORT } = require("./constants");
 
+const connect = function() {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
   });
 
   conn.on("connect", () => {
     console.log("connected succesfully");
+    conn.write("Name: ESL");
   });
 
   conn.on("data", (data) => {
     console.log(data);
   });
-
-  conn.on("connect", () => {
-    conn.write("Name: ESL");
-    // setTimeout(() => conn.write("Move: up"), 50);
-    // setTimeout(() => conn.write("Move: left"), 100);
-    // setTimeout(() => conn.write("Move: down"), 150);
-    // setTimeout(() => conn.write("Move: right"), 200);
-    
-  });
-
-  
 
   conn.setEncoding("utf8");
 
